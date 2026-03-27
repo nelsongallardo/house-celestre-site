5c1577266c19cde28cbb102982cf7130ccfafae7
linzakarauskaite-maker <lin.zakarauskaite@gmail.com>
2026-03-27 13:03:38 +0200

    Update page.tsx
    Text changed: House Celestre is being built

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main className="min-h-screen flex items-center justify-center bg-white">
-      <h1 className="text-black text-4xl">Coming soon</h1>
+      <h1 className="text-black text-4xl">House Celestre is being built</h1>
     </main>
   )
 }
